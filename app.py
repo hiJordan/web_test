c5f0c81cc1ab0221b35f6d2aa3b67408c9af83ed
wd <1642297507@qq.com>
2019-11-23 23:50:01 +0800

    host修改以便所有人访问

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ app.register_blueprint(todo_routes, url_prefix='/todo')
 
 if __name__ == '__main__':
     config = dict(
-        debug=True,
+        host='0.0.0.0',
         port=3000,
     )
     app.run(**config)
